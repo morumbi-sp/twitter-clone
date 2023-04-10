f16adec14136a86c3f1f41f739f6d4f4c50ca254
Morumbi <zun0323@gmail.com>
2023-04-10 13:12:40 -0300

    css / Index

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        banana: {
+          DEFAULT: '#FEB202',
+        },
+        myText: {
+          light: '#6b7280',
+          medium: '#4b5563',
+          dark: '#374151',
+          darkest: '#1f2937',
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 };
